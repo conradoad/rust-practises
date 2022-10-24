5065cd4734662e3b2178384ab6bbaf2090fa06e6
Conrado Costa <conradocostaad@yahoo.com.br>
2022-10-24 01:59:45 -0300

    Learning variables and mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    muttable();
+    shaddowing();
+}
+
+fn muttable() {
+    println!("Exemple 1");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn shaddowing() {
+    println!("Exemple 2");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
