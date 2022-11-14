a691d7d1477435ef6f2b7bd744faeab2a44f8543
Conrado Costa <conradocostaad@yahoo.com.br>
2022-11-14 00:21:46 -0300

    Learning functions (finishing)

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,7 +3,13 @@ fn main() {
 
     // another_function(5);
 
-    print_labeled_measurements(5, 'h');
+    // print_labeled_measurements(5, 'h');
+
+    // let x = five();
+    // println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x: i32) {
@@ -12,4 +18,12 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurements(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
